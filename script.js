77ec5fd59c9b5cce873fd4a71ef7711be5eb3ba7
elevate-ethan <ethan@elevateclub.org>
2024-02-23 18:33:34 -0500

    draw frame and tmino

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function buttonclicked() {
     document.querySelector("#answer").innerHTML="iglooes it togther"
-                }
+}
+
+var canvas = document.querySelector("#canvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(0,0,100,200);
+ctx.stroke();
+
+function tmino(x,y) {
+    ctx.beginPath();
+    ctx.rect(x,y,10,10);
+    ctx.rect(x+10,y,10,10);
+    ctx.rect(x+20,y,10,10);
+    ctx.rect(x+10,y+10,10,10);
+    ctx.stroke();                       
+}
+
+tmino(40,90)
